aeea54459af93e26cb1cbda6eddfb30cb1761c0a
adhammhaydat <adhammohidat123@gmail.com>
2021-10-17 14:14:50 +0300

    to team

---
+++ game_of_greed/game_logic.py
@@ -0,0 +1,22 @@
+from random import randint
+
+class GameLogic:
+    def __init__(self):
+        pass
+    @staticmethod
+    def calculate_score(cls,dice_roll):
+        return tuple(randint(1,6) for _ in range(0,num_dice))
+    @staticmethod    
+    def roll_dice(cls,num):
+        return tuple(randint(1,6) for _ in range(0,num_dice))
+    
+
+class Banker():
+    def __init__(self) :
+        pass
+    def shelf(self):
+        pass
+    def banik(self):
+        pass
+    def clear_shelf(self):
+        pass
